9a7c857d8f9451f42b0504b0340199a26004a16c
Rinka <146360541+Rinka-Dokuta@users.noreply.github.com>
2024-09-16 08:55:32 -0600

    Create if statement review.py

---
+++ if statement review.py
@@ -0,0 +1,9 @@
+choice = input("Do you prefer fish, dogs, or cats?")
+if choice == "fish":
+    print("fish noises")
+elif choice == "dogs":
+    print("Bark!")
+elif choice == "cats":
+    print("Meow")
+else:
+    print ("That's not an option >:(")
